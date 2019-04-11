309cdad7611a6ed054b6db411fe54aa8d697632d
alex <alex@staticlibs.net>
2019-04-11 19:14:57 +0100

    import wilton 31597a8

--- wilton/kiosk.js
+++ wilton/kiosk.js
@@ -44,14 +44,17 @@
  */
 define([
     "./dyload",
+    "./misc",
     "./wiltoncall",
     "./utils"
-], function(dyload, wiltoncall, utils) {
+], function(dyload, misc, wiltoncall, utils) {
     "use strict";
 
-    dyload({
-        name: "wilton_kiosk"
-    });
+    if (misc.isLinux()) {
+        dyload({
+            name: "wilton_kiosk"
+        });
+    }
 
     /**
      * @function run
@@ -61,6 +64,10 @@ define([
      * Opens a WebView window with a specified URL to load.
      * 
      * Current thread remains blocked until WebView window is closed.
+     *
+     * Note: on Linux it is better to run kiosk on `duktape` JS engine,
+     * on Windows `rhino` or `nashorn` JS engine must be used because
+     * on Windows kiosk relies on OpenJFX. 
      * 
      * @param options `Object` configuration object, see possible options below
      * @param callback `Function|Undefined` callback to receive result or error
@@ -73,19 +80,49 @@ define([
      *  - __fullscreenKey__ `Number|Undefined` keyboard key to switch WebView window
      *                   between full-screen and non-full-screen modes,
      *                   for GTK+ backend see keys mapping: https://gitlab.gnome.org/GNOME/gtk/raw/master/gdk/gdkkeysyms.h ,
-     *                   default value: `F11`
+     *                   default value on Linux: `F11`; non-configurable on Windows, default value: `Esc`
      *  - __closeKey__ `Number|Undefined` keyboard key to close the WebView window
      *                   for GTK+ backend see keys mapping: https://gitlab.gnome.org/GNOME/gtk/raw/master/gdk/gdkkeysyms.h ,
-     *                   default value: `Escape`
+     *                   default value: `Escape`; not supported on Windows
      *  - __windowWidth__ `Number|Undefined` window width in pixels, default value: `640`
      *  - __windowHeight__ `Number|Undefined` window height in pixels, default value: `480`
      *  - __consoleToStdout__ `Boolean|Undefined` redirect `console.log()` messages to STDOUT,
-     *                        default value: `false`
+     *                        default value: `false`; not supported on Windows
      */
     function run(options, callback) {
         var opts = utils.defaultObject(options);
         try {
-            wiltoncall("kiosk_run", opts);
+            if (misc.isLinux()) {
+                wiltoncall("kiosk_run", opts);
+            } else if (misc.isWindows()) {
+                var Runnable = Packages.java.lang.Runnable;
+                var SimpleStringProperty = Packages.javafx.beans.property.SimpleStringProperty;
+                var Image = Packages.javafx.scene.image.Image;
+                var Scene = Packages.javafx.scene.Scene;
+                var Platform = Packages.javafx.application.Platform;
+                var WebView = Packages.javafx.scene.web.WebView;
+                var FxApp = Packages.wilton.support.fx.FxApp;
+
+                utils.checkPropertyType(opts, "url", "string");
+
+                FxApp.launchApp(new Runnable({run: function() {
+                    var stage = FxApp.STAGE;
+                    var webView = new WebView();
+                    webView.getEngine().load(opts.url);
+                    var scene = new Scene(webView);
+                    stage.setScene(scene);
+                    var titleProp = new SimpleStringProperty("wilton");
+                    stage.getIcons().add(new Image("file:" + misc.wiltonConfig().wiltonHome + "conf/logo.png"));
+                    stage.titleProperty().bind(titleProp);
+                    stage.setFullScreen(true === opts.fullscreen);
+                    stage.setWidth("number" === typeof(opts.windowWidth) ? opts.windowWidth : 640);
+                    stage.setHeight("number" === typeof(opts.windowHeight) ? opts.windowHeight : 480);
+                    stage.show();
+                }}));
+                Platform.exit();
+            } else {
+                throw new Error("Unsupported OS");
+            }
             utils.callOrIgnore(callback);
         } catch (e) {
             utils.callOrThrow(callback, e);
