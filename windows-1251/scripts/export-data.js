fca422122cdd7a3aaaadbef2ea82f32e428a428a
alex <alex@staticlibs.net>
2019-09-07 19:40:17 +0100

    import windows-1251 6224a2e

---
+++ windows-1251/scripts/export-data.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var jsesc = require('jsesc');
+
+function readJSON(path) {
+	return JSON.parse(fs.readFileSync(path, 'utf-8'));
+}
+
+module.exports = {
+	'labels': jsesc(readJSON('data/labels.json'), {
+		'compact': false,
+		'indentLevel': 2
+	}),
+	'encoded': jsesc(readJSON('data/encoded.json'), { 'wrap': true }),
+	'decoded': jsesc(readJSON('data/decoded.json'), { 'wrap': true }),
+	'indexByCodePoint': jsesc(readJSON('data/index-by-code-point.json')),
+	'indexByPointer': jsesc(readJSON('data/index-by-pointer.json')),
+	'version': readJSON('package.json').version
+};
