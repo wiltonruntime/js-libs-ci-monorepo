91a35a1b89821b68efd8fa4d2b1e43c8b6fac7df
alex <alex@staticlibs.net>
2019-09-18 16:57:13 +0100

    remove collections module

--- collections/shim.js
+++
@@ -1,9 +0,0 @@
-define(function(localRequire, exports, module) { var requireOrig = require; require = localRequire;
-
-var Array = require("collections/shim-array");
-var Object = require("collections/shim-object");
-var Function = require("collections/shim-function");
-var RegExp = require("collections/shim-regexp");
-
-
-require = requireOrig;});
--- collections/weak-map.js
+++
@@ -1,5 +0,0 @@
-define(function(localRequire, exports, module) { var requireOrig = require; require = localRequire;
-
-module.exports = (typeof WeakMap !== 'undefined') ? WeakMap : require("weak-map");
-
-require = requireOrig;});
